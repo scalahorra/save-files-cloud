b93ea6831f21a2bb91735c227de82951bb7d4c2f
Sergio Calahorra <sergiocalahorra20@gmail.com>
2023-09-09 10:56:56 +0200

    Save uploaded files to a collection

--- src/Api/FileUpload.tsx
+++ src/Api/FileUpload.tsx
@@ -1,6 +1,6 @@
-import { app, storage, database } from '@/firebaseConfig';
+import { storage } from '@/firebaseConfig';
 import { ref, getDownloadURL, uploadBytesResumable } from 'firebase/storage';
-import { NextApiRequest, NextApiResponse } from 'next';
+import { addFiles } from './Firestore';
 
 export const fileUpload = (file: any) => {
   const storageRef = ref(storage, `files/${file.name}`);
@@ -10,13 +10,13 @@ export const fileUpload = (file: any) => {
     console.log('snapshot',snapshot);
     const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
     console.log(progress);
+
   }, (error) => {
     alert(error);
+
   }, () => {
-    console.log('Termino')
     getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-      console.log(downloadURL);
-      // resizeBy.status(201).json(downloadURL);
+      addFiles(downloadURL)
     });
   });
 }
---
+++ src/Api/Firestore.tsx
@@ -0,0 +1,15 @@
+import { database } from "@/firebaseConfig";
+import { collection, addDoc } from "firebase/firestore";
+
+let files = collection(database, 'files');
+
+export const addFiles = (imageLink: string) => {
+  try {
+    addDoc(files, {
+      imageLink: imageLink
+    });
+
+  } catch (error) {
+    console.error(error);
+  }
+}
